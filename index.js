2005b2a213bf93cd2f61d8e058e6b6a713644bbc
Tony Eder <tonyeder11@gmail.com>
2023-10-10 13:34:38 -0700

    tests passed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Number.parseInt((Math.random() *10) + 1, 10);
+
+let num3 = 16;
+let num4 = 12;
+let mod = num3 % num4;
+let max = Math.max(10, 15, 20);
+
+
+
